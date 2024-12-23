aa678ffee78bed30325b715c5d7b6e1f645e80d9
zzzzshawn <zzzzshawn@gmail.com>
2024-12-23 17:11:10 +0530

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 nextConfig.images = {
-    domains: [],
-    remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: '**',
-        },
-    ],
+  domains: [],
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "**",
+    },
+  ],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
 };
 export default nextConfig;
