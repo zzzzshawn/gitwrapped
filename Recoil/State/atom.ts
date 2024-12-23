6a3a9fb63a8d1e9c93aa9e04c9bf094e6f753259
zzzzshawn <zzzzshawn@gmail.com>
2024-12-23 17:46:37 +0530

    ?

--- Recoil/State/atom.ts
+++ Recoil/State/atom.ts
@@ -23,5 +23,5 @@ export const graphState = atom<any>({
 
 export const backgroundState = atom<string>({
   key: 'backgroundState',
-  default: "https://gitwrapped-psi.vercel.app/public/assets/black.png"
+  default: "app/assets/black.png"
 })
